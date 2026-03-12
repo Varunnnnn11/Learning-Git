30de1af0e670c9035bcad9c9da29e77ff53cfc66
Varun Kapoor <varunkapoor1105@Varuns-MacBook-Air.local>
2026-03-12 16:27:20 +0530

    Adding Hello.java

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+import java.sql.SQLOutput;
+
+class Hello {
+    public static void main(String [] args){
+
+        System.out.println("Hello Worldddddddddd");
+        System.out.println("Usinggg Gittt");
+}
+        }
